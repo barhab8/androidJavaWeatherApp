0121d01f4756a1b3bf919df71b649d2e01d5320f
barr <barrrhab8@gmail.com>
2025-03-01 18:44:30 +0200

    added foundation to notifications

--- app/src/main/java/com/example/weatherapp/ui/activitys/SettingsActivity.java
+++ app/src/main/java/com/example/weatherapp/ui/activitys/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.example.weatherapp.ui.activitys;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 
